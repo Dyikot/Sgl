320e095a5313c464ea3e59163840ab1f6933c415
Dyikot <dimak.13dimak@gmail.com>
2024-12-28 21:42:17 +0500

    Event update

--- Sgl/Events/Event.h
+++ Sgl/Events/Event.h
@@ -15,19 +15,48 @@ namespace Sgl
 		using Object = TObject;
 		using EventArgs = TEventArgs;
 	public:
-		EventHandler(auto&& handler):
-			Base(std::forward<decltype(handler)>(handler))
+		EventHandler() = default;	
+		
+		template<typename THandler> requires std::constructible_from<Base, THandler>
+		EventHandler(THandler&& handler):
+			Base(std::forward<THandler>(handler))
 		{}
 
-		EventHandler& operator=(auto&& handler)
+		EventHandler(const EventHandler& handler):
+			Base(handler)
+		{}
+		
+		EventHandler(EventHandler&& handler):
+			Base(std::move(handler))
+		{}
+
+		template<typename THandler> requires std::constructible_from<Base, THandler>
+		EventHandler& operator=(THandler&& handler)
 		{
-			Base::operator=(std::forward<decltype(handler)>(handler));
+			Base::operator=(std::forward<THandler>(handler));
+			return *this;
+		}
+
+		EventHandler& operator=(const EventHandler& handler)
+		{
+			Base::operator=(handler);
+			return *this;
+		}
+
+		EventHandler& operator=(EventHandler&& handler)
+		{
+			Base::operator=(std::move(handler));
 			return *this;
 		}
 
 		bool operator==(const EventHandler& eventHandler) const noexcept
 		{
 			return this->target_type() == eventHandler.target_type();
+		}
+
+		bool operator!=(const EventHandler& eventHandler) const noexcept
+		{
+			return !operator==(eventHandler);
 		}
 	};
 
@@ -41,20 +70,29 @@ namespace Sgl
 	protected:
 		std::forward_list<THandler> _handlers;
 	public:
-		void operator+=(auto&& handler) 
-		{ 
-			_handlers.emplace_front(std::forward<decltype(handler)>(handler));
+		void Clear() noexcept { _handlers.clear(); }
+
+		void operator+=(THandler&& handler)
+		{
+			_handlers.emplace_front(std::forward<THandler>(handler));
 		}
 
-		void operator-=(auto&& handler) 
-		{ 
-			_handlers.remove(std::forward<decltype(handler)>(handler));
+		void operator+=(const THandler& handler)
+		{
+			_handlers.emplace_front(handler);
 		}
 
-		operator bool() const noexcept
-		{ 
-			return !_handlers.empty();
+		void operator-=(THandler&& handler)
+		{
+			_handlers.remove(std::forward<THandler>(handler));
 		}
+
+		void operator-=(const THandler& handler)
+		{
+			_handlers.remove(handler);
+		}
+
+		operator bool() const noexcept { return !_handlers.empty(); }
 
 		void operator()(THandler::Object* sender, const THandler::EventArgs& e) const
 		{
@@ -63,17 +101,5 @@ namespace Sgl
 				handler(sender, e);
 			}
 		}
-
-		void Clear() noexcept
-		{ 
-			_handlers.clear(); 
-		}
 	};	
-
-	/*template <typename THandler>
-	static bool operator==(const std::function<THandler>& lhs, 
-						   const std::function<THandler>& rhs) noexcept
-	{
-		return lhs.target_type() == rhs.target_type();
-	}*/
 }
